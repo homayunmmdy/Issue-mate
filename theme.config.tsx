ea92c47989470bd313a1cae46ccc07dbba038bc3
Homayoun <homayoun763@gmail.com>
2024-05-03 01:01:27 +0330

    speed andrew tate

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span> Interview Questions</span>,
+  logo: <span> My Note</span>,
   project: {
     link: 'https://github.com/homayunmmdy',
   },
