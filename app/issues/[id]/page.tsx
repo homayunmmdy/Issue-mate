6834d92bd0885e7023ae30a279a6c6cd39698d20
Homayoun <homayoun763@gmail.com>
2025-08-05 23:45:12 +0330

    Securing the Application

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,14 +1,17 @@
+import authOptions from "@/app/auth/authOptions";
 import prisma from "@/prisma/client";
 import { Box, Flex, Grid } from "@radix-ui/themes";
+import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
+import DeleteIssueButton from "./DeleteIssueButton";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
-import DeleteIssueButton from "./DeleteIssueButton";
 interface Props {
   params: { id: string };
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions);
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -22,10 +25,12 @@ const IssueDetailPage = async ({ params }: Props) => {
       <Box className="lg:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      <Flex direction='column' gap="4">
-        <EditIssueButton issueId={issue.id} />
-        <DeleteIssueButton issueId={issue.id}/>
-      </Flex>
+      {session && <Box>
+        <Flex direction="column" gap="4">
+          <EditIssueButton issueId={issue.id} />
+          <DeleteIssueButton issueId={issue.id} />
+        </Flex>
+      </Box>}
     </Grid>
   );
 };
