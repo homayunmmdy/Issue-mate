cf1c1dd7d8e6a2a06c0efdfe45715ffd5bf269e9
Homayoun <homayoun763@gmail.com>
2025-08-08 02:54:02 +0330

    Filtering Issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,10 +1,22 @@
+import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
+import { Status } from "@prisma/client";
 import { Table } from "@radix-ui/themes";
-import {IssueStatusBadge , Link} from '@/app/components'
 import IssueActions from "./IssueActions";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: { status: Status };
+}
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+  const issues = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
 
   return (
     <div>
@@ -44,6 +56,6 @@ const IssuesPage = async () => {
   );
 };
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export default IssuesPage;
