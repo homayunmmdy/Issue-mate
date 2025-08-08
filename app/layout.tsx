1550452f53f74d1ceb81fe4c06ef1cc1f823caf2
Homayoun <homayoun763@gmail.com>
2025-08-08 18:12:29 +0330

    change : accentColor

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <body className={inter.variable}>
         <QueryClientProvider>
           <AuthProvider>
-            <Theme appearance="light" accentColor="violet">
+            <Theme accentColor="red">
               <NavBar />
               <main className="p-5">
                 <Container>{children}</Container>
