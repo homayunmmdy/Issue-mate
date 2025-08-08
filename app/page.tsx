06f5c7dd018cc3159131788b9a62b133cc8c17ee
Homayoun <homayoun763@gmail.com>
2025-08-08 13:19:07 +0330

    Building the IssueSummary Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import LatestIssues from './LatestIssues'
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 
-const HomePage = () => {
-  return (
-    <LatestIssues />
-  )
-}
+const HomePage = async () => {
+  const open = await prisma.issue.count({where: {status: 'OPEN'}})
+  const inProgress = await prisma.issue.count({where: {status: 'IN_PROGRESS'}})
+  const closed = await prisma.issue.count({where: {status: 'CLOSED'}})
+  return <IssueSummary open={open} inProgress={inProgress} closed={closed}/>;
+};
 
-export default HomePage
+export default HomePage;
