66e60067897586e062d6fbb299c8d9550d9f9278
Homayoun <homayoun763@gmail.com>
2025-08-08 18:07:05 +0330

    add : space from Status Actions with table in issue list page

--- app/issues/list/loading.tsx
+++ app/issues/list/loading.tsx
@@ -1,11 +1,11 @@
 import { Skeleton } from "@/app/components";
-import { Table } from "@radix-ui/themes";
+import { Flex, Table } from "@radix-ui/themes";
 import IssueActions from "./IssueActions";
 
 const loadingIssuesPage = () => {
   const issues = [1, 2, 3, 4, 5];
   return (
-    <div>
+    <Flex direction="column" gap='3'>
       <IssueActions />
       <Table.Root variant="surface">
         <Table.Header>
@@ -38,7 +38,7 @@ const loadingIssuesPage = () => {
           ))}
         </Table.Body>
       </Table.Root>
-    </div>
+    </Flex>
   );
 };
 
