1d7e4762dc9011b98d4d696544a3506456635cf0
Homayoun <homayoun763@gmail.com>
2025-08-08 13:31:28 +0330

    Building the BarChart Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import prisma from "@/prisma/client";
-import IssueSummary from "./IssueSummary";
+import IssueChart from "./IssueChart";
 
 const HomePage = async () => {
-  const open = await prisma.issue.count({where: {status: 'OPEN'}})
-  const inProgress = await prisma.issue.count({where: {status: 'IN_PROGRESS'}})
-  const closed = await prisma.issue.count({where: {status: 'CLOSED'}})
-  return <IssueSummary open={open} inProgress={inProgress} closed={closed}/>;
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
 };
 
 export default HomePage;
